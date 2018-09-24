edff324ae8401ec481ff90ca0acefbe7fbd073cb
Jason Berlinsky <jason@barefootcoders.com>
2018-09-24 11:56:29 -0400

    Add support for Sinatra settings when reading environment

--- lib/resque/kubernetes/job.rb
+++ lib/resque/kubernetes/job.rb
@@ -83,7 +83,9 @@ module Resque
 
       # A before_enqueue hook that adds worker jobs to the cluster.
       def before_enqueue_kubernetes_job(*_)
-        if defined? Rails
+        if defined?(settings)
+          return unless Resque::Kubernetes.environments.include?(settings.environment.to_s)
+        elsif defined?(Rails)
           return unless Resque::Kubernetes.environments.include?(Rails.env)
         end
 
